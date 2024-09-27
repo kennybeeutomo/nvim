a0474eb98b353d556aae4e4ba28947f1c4261b76
kennybeeutomo <kennybee132@gmail.com>
2024-09-27 20:36:16 +0700

    Changed pwsh shell options

--- lua/config/before/options.lua
+++ lua/config/before/options.lua
@@ -11,10 +11,13 @@ vim.cmd([[
 ]])
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-	vim.opt.shell = "pwsh"
-	vim.opt.shellcmdflag = "-command"
-	vim.opt.shellquote = '\"'
-	vim.opt.shellxquote = ''
+	vim.cmd([[
+	let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+	let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+	let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+	let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+	set shellquote= shellxquote=
+	]])
 end
 
 vim.opt.fillchars = { eob = ' ' }
