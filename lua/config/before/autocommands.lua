48d8aea6ece501098342b5545a96116d61cafdc7
kennybeeutomo <kennybee132@gmail.com>
2024-11-12 11:18:37 +0700

    Fixed lualine not refreshing

--- lua/config/before/autocommands.lua
+++ lua/config/before/autocommands.lua
@@ -34,3 +34,16 @@ autocmd({'ColorScheme'}, {
 	end,
 	desc = "Load lualine theme based on the chosen colorscheme"
 })
+
+-- The events on which lualine redraws itself
+-- from https://github.com/nvim-lualine/lualine.nvim/issues/1293#issuecomment-2280456044
+local default_refresh_events = {"WinEnter","BufEnter","BufWritePost","SessionLoadPost","FileChangedShellPost","VimResized","Filetype","CursorMoved","CursorMovedI","ModeChanged"}
+local lualine_conf = augroup('lualine_conf', { clear = true })
+autocmd(default_refresh_events, {
+	pattern = '*',
+	group = lualine_conf,
+	callback = function()
+		require("lualine").refresh()
+	end,
+	desc = "Refresh lualine on some events"
+})
