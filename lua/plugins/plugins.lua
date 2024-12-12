a9e61fa75bac2819ac9aecc439d11932802c9d39
kennybeeutomo <kennybee132@gmail.com>
2024-12-12 16:20:22 +0700

    Fixed LuaSnip build command

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -112,7 +112,7 @@ require("lazy").setup({
 	{
 		"L3MON4D3/LuaSnip",
 		version = "v2.*",
-		build = "make install_jsregexp"
+		build = "make install_jsregexp CC=gcc.exe SHELL=C:/Program\\ Files/Git/bin/sh.exe .SHELLFLAGS=-c"
 	},
 	{ "saadparwaiz1/cmp_luasnip" },
 
