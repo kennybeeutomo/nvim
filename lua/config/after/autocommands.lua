152d2fef2ac669069e8d280cecf9f47b0318277b
kennybeeutomo <kennybee132@gmail.com>
2025-07-23 15:45:01 +0700

    fix autoViews QuitPre + removed lualine autocmd

--- lua/config/after/autocommands.lua
+++ lua/config/after/autocommands.lua
@@ -74,19 +74,10 @@ autocmd({"BufWritePost"}, {
 })
 autocmd({"QuitPre"}, {
 	group = autoViews,
-	command = "mkview",
-	desc = "Save folds on quit"
-})
-
--- The events on which lualine redraws itself
--- from https://github.com/nvim-lualine/lualine.nvim/issues/1293#issuecomment-2280456044
-local default_refresh_events = {"WinEnter","BufEnter","BufWritePost","SessionLoadPost","FileChangedShellPost","VimResized","Filetype","CursorMoved","CursorMovedI","ModeChanged"}
-local lualine_conf = augroup("lualine_conf", { clear = true })
-autocmd(default_refresh_events, {
-	pattern = "*",
-	group = lualine_conf,
 	callback = function()
-		require("lualine").refresh()
+		if vim.api.nvim_buf_get_name(0) ~= "" then
+			vim.cmd("mkview")
+		end
 	end,
-	desc = "Refresh lualine on some events"
+	desc = "Save folds on quit"
 })
