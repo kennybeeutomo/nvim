d62cfdb5cd9ef4becf9aae19138045aece244f4c
kennybeeutomo <kennybee132@gmail.com>
2026-02-25 00:28:09 +0700

    lazydev: fix plugin not working on first buffer
    See https://github.com/folke/lazydev.nvim/issues/136.
    Downgrade lua_ls to 3.16.4 to fix it

--- lua/plugins/mason/init.lua
+++ lua/plugins/mason/init.lua
@@ -4,7 +4,7 @@ function M.setup()
 	require("mason").setup()
 	require("mason-lspconfig").setup({
 		ensure_installed = {
-			"lua_ls",
+			"lua_ls@3.16.4", -- use this version to prevent lazydev issue 136
 			"clangd",
 			"marksman",
 			-- "clang-format",
